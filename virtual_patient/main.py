90534841419f1b6bd016e1ecd25a9b9ef67b0dfd
Hyonyoung Choi <hyonchoi@seas.upenn.edu>
2025-09-01 15:19:59 -0400

    adjust topic names

--- virtual_patient/main.py
+++ virtual_patient/main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import sys, os, time, json
 import numpy as np
 from scipy.integrate import solve_ivp
@@ -11,30 +14,32 @@ from meals_model import Meals
 from insulin_model import Insulin
 from bergman_model import Bergman
 from mqtt import MQTT
-        
-        
+
+
 class VP_MQTT(MQTT):
     def __init__(self, host, port, username, password, topics):
         super().__init__(host, port, username, password)
         self.time_step = 0
         self.topics = topics
+
+        self.disp_interval = 1  # display interval in seconds
         
         self.stated = False
 
-        self.client.message_callback_add(f'{topics["TB_VP_ATTRIBUTE_TOPIC"]}/#', self.on_message_profile)
+        self.client.message_callback_add(f'{topics["VP_ATTRIBUTE_TOPIC"]}/#', self.on_message_profile)
         self.client.message_callback_add(topics["INSULIN_TOPIC"], self.on_message_insulin)
         self.connect()
         
     def on_connect(self, client, userdata, flags, reason_code, properties):
         print(f">>> Connected with result code {reason_code}!")
         # Subscribe to the topics
-        client.subscribe(self.topics['TB_VP_ATTRIBUTE_TOPIC'], qos=1)
-        client.subscribe(f'{self.topics["TB_VP_ATTRIBUTE_TOPIC"]}/response/+', qos=1)
-        client.subscribe(f'{self.topics["TB_VP_RPC_TOPIC"]}/request/+', qos=1)
+        client.subscribe(self.topics['VP_ATTRIBUTE_TOPIC'], qos=1)
+        client.subscribe(f'{self.topics["VP_ATTRIBUTE_TOPIC"]}/response/+', qos=1)
         client.subscribe(self.topics["INSULIN_TOPIC"], qos=1)
+
         # Request the virtual patient profile
         print('>>> Requesting Virtual Patient Profile...')
-        client.publish(f'{self.topics["TB_VP_ATTRIBUTE_TOPIC"]}/request/1', '{"sharedKeys":"PatientProfile"}', qos=1)
+        client.publish(f'{self.topics["VP_ATTRIBUTE_TOPIC"]}/request/1', '{}', qos=1)
         
     def on_message_profile(self, client, userdata, message):
         """
@@ -45,41 +50,58 @@ class VP_MQTT(MQTT):
             return
         
         payload = json.loads(message.payload.decode('utf-8'))
+
+        if 'PatientProfile' not in payload:
+            print("Warning: No PatientProfile found in the payload.")
+            return
+
+        profile = payload['PatientProfile']
+
         # Update patient type
-        diabetic = payload['shared']['PatientProfile']['diabetic'] if 'shared' in payload else payload['PatientProfile']['diabetic']
+        diabetic = profile['diabetic']
         self.patient_type = 'diabetic' if diabetic else 'normal'
+
         # Update the meal schedule
-        meals = payload['shared']['PatientProfile']['meals'] if 'shared' in payload else payload['PatientProfile']['meals']
+        meals = profile['meals']
         self.meals = []
         for meal in meals:
             self.meals.append((float(meal['time']), float(meal['carbs']), float(meal['duration'])))
         self.meal_function = Meals(self.meals)
+
         # Upadta the bolus insulins
-        bolus_insulins = payload['shared']['PatientProfile']['bolus_insulins'] if 'shared' in payload else payload['PatientProfile']['bolus_insulins']
+        bolus_insulins = profile['bolus_insulins']
         self.bolus_insulins = []
         for insulin in bolus_insulins:
             self.bolus_insulins.append((float(insulin['time']), float(insulin['dose']), float(insulin['duration'])))
         self.insulin_function = Insulin(bolus_insulin=self.bolus_insulins)
+
         print(f">>> Virtual Patient Profile Recieved: \n{self.patient_type=}, \n{self.meals=}, \n{self.bolus_insulins=}")
+
         # Initialize the Bergman model
         self.bergman = Bergman(type=self.patient_type, meals=self.meal_function, insulin=self.insulin_function)
+
         # Update bergman parameters
-        params = payload['shared']['PatientProfile']['bergman_param'] if 'shared' in payload else payload['PatientProfile']['bergman_param']
+        params = profile['bergman_param']
         self.bergman.update_params(params)
+
         print(f">>> Customized Bergman Parameters: \n{params=}")
         
         # Simulation settings, initialize once, do not update
-        if 'shared' in payload:
-            self.disp_interval = float(payload['shared']['PatientProfile']['sim_settings']['disp_interval'])
-            self.simu_interval = int(payload['shared']['PatientProfile']['sim_settings']['simu_interval'])
-            self.simu_length = int(payload['shared']['PatientProfile']['sim_settings']['simu_length'])
-            init_state = payload['shared']['PatientProfile']['sim_settings']['init_state']
+        if 'sim_settings' in profile:
+            sim_settings = profile['sim_settings']
+
+            self.disp_interval = float(sim_settings['disp_interval'])
+            self.simu_interval = int(sim_settings['simu_interval'])
+            self.simu_length = int(sim_settings['simu_length'])
+            init_state = sim_settings['init_state']
             self.init_state = [float(init_state['G0']), float(init_state['X0']), float(init_state['I0'])]
             
             # Simulation storage
             self.solution = np.zeros((self.simu_length, 3))
+
             # Initial conditions
             self.solution[0, :] = self.init_state
+
             print(f">>> Simulation Settings: \n{self.disp_interval=}, \n{self.simu_interval=}, \n{self.simu_length=}, \n{self.init_state=}")
             
         self.stated = True
@@ -97,8 +119,8 @@ class VP_MQTT(MQTT):
             print("Press CTRL+C to exit the simulation loop...")
             self.loop_start()
             while True:
-                disp_interval = self.disp_interval if hasattr(self, 'disp_interval') else 1
-                time.sleep(disp_interval)
+                time.sleep(self.disp_interval)
+
                 if not self.stated:
                     continue
                 print('.', end='', flush=True)
@@ -111,18 +133,21 @@ class VP_MQTT(MQTT):
                 self.solution[self.time_step, :] = deepcopy(res.y[:, -1])
                 
                 # virtual CGM sensor, send to openAPS
-                self.client.publish(self.topics['CGM_TOPIC'], json.dumps({'Glucose': self.solution[self.time_step][0], 'time': self.time_step * self.simu_interval}), qos=1)
+                data = {
+                    'Glucose': self.solution[self.time_step][0],
+                    'time': self.time_step * self.simu_interval,
+                }
+                self.client.publish(self.topics['CGM_TOPIC'], json.dumps(data), qos=1)
                 
-                # ThingsBoard, just for display
+                # Dashboard for visualization
                 ts = int(datetime.now().timestamp() * 1000)
-                data = [{
-                    'ts': ts,
-                    'values': {
-                        'Insulin': self.solution[self.time_step][2],
-                        'Glucose': self.solution[self.time_step][0],
-                    }
-                }]
-                self.client.publish(self.topics['TB_VP_TELEMETRY_TOPIC'], json.dumps(data), qos=1)
+                data = {
+                    'timestamp': ts,
+                    'Insulin': self.solution[self.time_step][2],
+                    'Glucose': self.solution[self.time_step][0],
+                }
+                self.client.publish(self.topics['VP_TELEMETRY_TOPIC'], json.dumps(data), qos=1)
+
                 if self.time_step >= self.simu_length - 1:
                     print("\n>>> Simulation completed.")
                     break
@@ -142,13 +167,18 @@ def main():
     USERNAME = os.getenv('USERNAME')
     PASSWORD = os.getenv('PASSWORD')
 
+    team_name = os.getenv('TEAM_NAME')
+    if team_name is None or team_name == '':
+        print('Error: TEAM_NAME is not set in the environment variables.')
+        sys.exit(1)
+
+    topic_prefix = f'cis441-541/{team_name}'
+
     topics = {
-        'TOPIC_PREFIX': os.getenv('TOPIC_PREFIX'),
-        'TB_VP_ATTRIBUTE_TOPIC': os.getenv('TB_VP_ATTRIBUTE_TOPIC'),
-        'TB_VP_TELEMETRY_TOPIC': os.getenv('TB_VP_TELEMETRY_TOPIC'),
-        'TB_VP_RPC_TOPIC': os.getenv('TB_VP_RPC_TOPIC'),
-        'INSULIN_TOPIC': os.getenv('INSULIN_TOPIC'),
-        'CGM_TOPIC': os.getenv('CGM_TOPIC'),
+        'VP_ATTRIBUTE_TOPIC': f'{topic_prefix}/vp-attributes',
+        'VP_TELEMETRY_TOPIC': f'{topic_prefix}/vp-telemetry',
+        'INSULIN_TOPIC': os.getenv('INSULIN_TOPIC', f'{topic_prefix}/insulin-pump'),
+        'CGM_TOPIC': os.getenv('CGM_TOPIC', f'{topic_prefix}/cgm'),
     }
     print(f'>>> Topic settings from environment: \n{topics=}')
     
